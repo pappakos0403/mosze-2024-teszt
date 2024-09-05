c9ceec2463e626c1568944ea49639e66f8bba551
pappakos0403 <pappakos0403@gmail.com>
2024-09-05 12:12:29 +0200

    added comments

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //elírt változónév
+    std::cout << '1-100 ertekek duplazasa' //nincs ;
+    for (int i = 0;) //hiányos for loop
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hiányos for loop
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //nincs ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //nincs kezdőérték
+    for (int i = 0; i < N_ELEMENTS, i++) //, helyett ;
     {
-        atlag += b[i]
+        atlag += b[i] //nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
